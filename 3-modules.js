480ce144494098b27efc1e64b1a0d80f74a9fe53
semaerol <semaerol543@gmail.com>
2025-08-28 10:04:50 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const sayHi = require('./5-utils')
+const names = require('./4-names')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+console.log(data)  
+
+sayHi(names.susan)
+sayHi(names.peter)
+sayHi(names.john)
